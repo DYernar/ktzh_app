39f4d5eb1e4707291c3e999e45759235b8d4038b
DYernar <eduisebai.1@gmail.com>
2021-03-13 04:02:58 +0600

    profile page

--- lib/navigation/app.dart
+++ lib/navigation/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ktzh_app/views/main_page.dart';
 import 'package:ktzh_app/views/product_page.dart';
+import 'package:ktzh_app/views/profile_page.dart';
 
 class App extends StatefulWidget {
   App({Key key}) : super(key: key);
@@ -17,16 +18,41 @@ class _AppState extends State<App> {
       home: ScreenUtilInit(
         designSize: Size(1080, 2340),
         allowFontScaling: true,
-        builder: () => MaterialApp(
-          routes: {
-            '/': (BuildContext context) => MainPage(),
-            '/product_page': (BuildContext context) => ProductPage()
-          },
-          initialRoute: '/',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
+        builder: () => Scaffold(
+          appBar: AppBar(),
+          drawer: drawer(),
+          body: MaterialApp(
+            routes: {
+              '/': (BuildContext context) => MainPage(),
+              '/product_page': (BuildContext context) => ProductPage(),
+              '/profile_page': (BuildContext context) => ProfilePage(),
+            },
+            initialRoute: '/',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget drawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Container(
+              child: Image.asset('/assets/images/ktzh.svg'),
+            ),
+          ),
+          ListTile(title: Text('Profile')),
+          Divider(thickness: ScreenUtil().setHeight(2)),
+          ListTile(title: Text('Profile')),
+          Divider(thickness: ScreenUtil().setHeight(2)),
+          ListTile(title: Text('Profile')),
+          Divider(thickness: ScreenUtil().setHeight(2)),
+        ],
       ),
     );
   }
